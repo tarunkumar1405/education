3aa1bbbeebe28204f9ca6f7b9b499c8f774afe83
Tarun Kumar <80479074+tarunkumar1405@users.noreply.github.com>
2021-08-28 13:47:18 +0530

    second

---
+++ myscript.js
@@ -0,0 +1,36 @@
+var text = "Spread Right Information!!! A little knowledge is a dangerous thing... ";
+
+var chars = text.split('');
+var container = document.getElementById("container");
+
+var i = 0;
+setInterval(function () {
+    if (i < chars.length) {
+        container.innerHTML += chars[i++];
+    } else {
+        i = 0;
+        container.innerHTML = "";
+    }
+}, 80);
+
+
+    //Get the button
+    var mybutton = document.getElementById("myBtn");
+    
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+    
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+    
